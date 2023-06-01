d753f23002f5e79a5e491dbd20a68dee65aeb743
karen <karenkaka2010@hotmail.com>
2023-05-31 21:41:51 -0300

    feat: change model

--- api/utils.py
+++ api/utils.py
@@ -16,19 +16,20 @@ class_list = ['nota-10', 'nota-2', 'nota-20', 'nota-200', 'nota-5', 'nota-50', '
 
 # Transformações a serem aplicadas de acordo com o modelo pretreinado do ImageNet
 train_transform = transforms.Compose([
-    transforms.Resize((225, 224)),
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
     transforms.ToTensor(),
-    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
 def load_model_trained():
-    model_path = 'models/my-model.pt'
-    get_file_from_s3('model-recognize-notes', 'vgg16_net-last.pt', model_path)        
-    model_vgg16 = models.vgg16_bn(pretrained=True)
+    model_path = 'models/model.pt'    
+    # Carrega o modelo pretreinado - sequeezenet
+    model = models.squeezenet1_1(pretrained=True)
     # Fine tunning - Subtitui a camada de classificação (última)
-    num_input = list(model_vgg16.children())[-1][-1].in_features # 4096 -> Ultimo elemento da ultima camada da rede (classifier)
-    new_classifier = list(model_vgg16.classifier.children())[:-1] # Ùltima camada - (menos) o último elemento (classifier)
-    new_classifier.append(nn.Linear(num_input, len(class_list))) # Anexo na última camada a minha camada de classficao adaptada
-    model_vgg16.classifier = nn.Sequential(*new_classifier) # Aplica ao modelo a nova camada de classificao
-    model_vgg16.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
-    return model_vgg16
+    # Fine tunning - Subtitui a camada de classificação (última)
+    num_input = list(model.children())[-1][-1].in_features # 4096 -> Ultimo elemento da ultima camada da rede (classifier)
+    model.classifier[1] = nn.Conv2d(512, num_input, kernel_size=(1, 1))
+
+    model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+    return model
